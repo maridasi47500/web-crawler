6a1a5114ad37da1bc72bee34eca770ac0ec6423f
pirate du bato <anonyme201504@gmail.com>
2024-08-29 23:19:19 +0200

    hey

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,11 +5,26 @@ class WelcomeController < ApplicationController
   end
 
   def google
-       b = Watir::Browser.new
-       b.goto 'http://google.com'
-       b.text_field(:name=>'q').set params[:s]
-       b.button(:name=>'btnG').click
-       b.h3s(:class=>'r')[0].click
+    Selenium::WebDriver::Chrome.path = '/usr/bin/chromium-browser'
+driver = Selenium::WebDriver.for :chrome
+b = Watir::Browser.new driver
+  form = b.form(action: '/search')
+
+b.goto 'http://google.com'
+b.button(:id=>'L2AGLb').click
+b.textarea(:name=>'q').value= params[:s]
+begin
+#form.input(value: 'Google Search').click
+  b.send_keys :enter
+  
+sleep(5)
+if params["image"] == "true"
+b.divs(:role=>'listitem')[1].click
+end
+rescue => e
+  puts e.message
+end
+
 
 
   end
